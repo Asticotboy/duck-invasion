fefab1c3bbe6db054a13f49d226cdf2362fcfedf
Asticotboy <c.ludwigs@outlook.fr>
2022-06-05 00:02:58 +0200

    Init and first upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# duck-invasion
+# Duck-Invasion
+@Asticotboy
+
 A chrome extension in which lots of ducks invade your browser
+
+# How To Install ?
+
+At first enable the developper mode in chrome://extensions/. (on the right top corner)
+Then click on the first button,labeled "Load Unpacted" on the bar that has just appeared.
+
+Select the folder and it's good. 
+Now the ducks will invade you
+
+
+
+
+
---
+++ coin.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', function (e) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = chrome.runtime.getURL("style.css");;
+    document.head.appendChild(link);
+}, false);
+
+var test ="test";
+
+function showCoin(obj){
+    for( i=0; i< obj.length; i++ )
+    {
+        var img = document.createElement("img");
+        img.src = chrome.runtime.getURL("images/logo512re.png");
+        img.style.width = "50px";
+        img.style.position = "absolute";
+           
+        obj[i].appendChild(img);
+    }
+}
+
+function replaceImg(){
+    var ims = document.getElementsByTagName("img");
+    for( i=0; i< ims.length; i++ )
+    {
+       ims[i].src = chrome.runtime.getURL("images/logo512re.png");
+    }
+}
+
+
+document.body.style.backgroundImage = "url('https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb1%2FMale_mallard_standing.jpg%2F220px-Male_mallard_standing.jpg&imgrefurl=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FCanard&tbnid=L4pWU9bTISaPLM&vet=12ahUKEwiJ1ordwJT4AhU_XvEDHYT7Au8QMygBegUIARDaAQ..i&docid=Ly7LaZkGQso-iM&w=220&h=165&q=canard&ved=2ahUKEwiJ1ordwJT4AhU_XvEDHYT7Au8QMygBegUIARDaAQ')";
+
+
+showCoin(document.getElementsByTagName("p"));
+showCoin(document.getElementsByTagName("h1"));
+showCoin(document.getElementsByTagName("h2"));
+showCoin(document.getElementsByTagName("h3"));
+replaceImg();
