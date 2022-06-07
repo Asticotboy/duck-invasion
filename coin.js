4f3433cbdf31ddb93a3d26d957272edacd05f270
Asticotboy <c.ludwigs@outlook.fr>
2022-06-07 17:03:10 +0200

    UPDATE

--- coin.js
+++ coin.js
@@ -28,15 +28,6 @@ function replaceImg(){
     }
 }
 
-function replaceImg(){
-    var tiles = document.getElementsByClassName("tile-inner");
-    for( i=0; i< tiles.length; i++ )
-    {
-       tiles[i].backgroundImage = `url(${chrome.runtime.getURL("images/logo512re.png")}) !important`
-    }
-}
-
-
 document.body.style.backgroundImage = "url('https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb1%2FMale_mallard_standing.jpg%2F220px-Male_mallard_standing.jpg&imgrefurl=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FCanard&tbnid=L4pWU9bTISaPLM&vet=12ahUKEwiJ1ordwJT4AhU_XvEDHYT7Au8QMygBegUIARDaAQ..i&docid=Ly7LaZkGQso-iM&w=220&h=165&q=canard&ved=2ahUKEwiJ1ordwJT4AhU_XvEDHYT7Au8QMygBegUIARDaAQ')";
 
 
@@ -45,10 +36,7 @@ setInterval(()=>{
     showCoin(document.getElementsByTagName("h1"));
     showCoin(document.getElementsByTagName("h2"));
     showCoin(document.getElementsByTagName("h3"));
-    try{
-        replaceTiles();
-    }
-    finally{}
+
     replaceImg();
 },500);
 
